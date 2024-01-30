a27571499ccf3c9945a19189d05ef818ce339d1e
Rune Christoffersen <rune1833@live.dk>
2024-01-30 11:14:02 +0100

    Tailwind correctly setup

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -6,6 +6,8 @@ export default function Index() {
   return (
     <div>
       <p className="bg-red-500">Hello</p>
+      <p className="bg-slate-500">HEj</p>
+      <p className="bg-green-500">Hello</p>
     </div>
   );
 }
